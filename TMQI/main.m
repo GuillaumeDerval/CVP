45a1aa0e086e9783a2dda664751eb0362cf7d324
barrel_shifter <kevin.lhoest@gmail.com>
2015-12-08 15:52:50 +0100

    premiere version du schema remplacé

--- TMQI/main.m
+++ TMQI/main.m
@@ -1,5 +1,5 @@
 hdrImage = hdrread('chateau.hdr');
 ldrImage = [imread('chateau0.jpg'),imread('chateau2.jpg')];
-rgb = tonemap(hdr);
-imshow(rgb);
-%[Q, S, N, s_maps, s_local] = TMQI(hdrImage, ldrImage)
+%rgb = tonemap(hdrImage);
+%imshow(rgb);
+[Q, S, N, s_maps, s_local] = TMQI(hdrImage, ldrImage)
