973375f8a7323451f1ac0387d680660cbbcdcf37
Anthony Gégo <anthonygego@hotmail.com>
2015-12-09 13:50:17 +0100

    delete tmqi

--- TMQI/main.m
+++
@@ -1,5 +0,0 @@
-hdrImage = hdrread('chateau.hdr');
-ldrImage = [imread('chateau0.jpg'),imread('chateau2.jpg')];
-%rgb = tonemap(hdrImage);
-%imshow(rgb);
-[Q, S, N, s_maps, s_local] = TMQI(hdrImage, ldrImage)
